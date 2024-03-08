0aeaea94bd10d2f3c18ea4c253f9fd039a82beac
feliperod0519@hotmail.com <juan_andres_rodriguez_0519@hotmail.com>
2024-03-08 08:35:03 -0500

    Initial commit

---
+++ app/src/main/java/com/feliperodcoding/marketapp/MyAdapter.java
@@ -0,0 +1,67 @@
+package com.feliperodcoding.marketapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    private List<Item> items;
+
+    public ItemClickListener clickListener;
+
+    public void setClickListener(ItemClickListener myListener){
+        this.clickListener = myListener;
+    }
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        ImageView imageView;
+        TextView title;
+        TextView description;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageview);
+            title = itemView.findViewById(R.id.title_txt);
+            description = itemView.findViewById(R.id.description_text);
+
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (clickListener!=null){
+                clickListener.onClick(view,getAdapterPosition());
+            }
+        }
+    }
+
+    public MyAdapter(List<Item> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout,parent,false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Item item = this.items.get(position);
+        holder.title.setText(item.getItemName());
+        holder.description.setText(item.getItemDesc());
+        holder.imageView.setImageResource(item.getItemImg());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+}
